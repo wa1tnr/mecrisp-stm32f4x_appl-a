61ea280766ee7375f7b102d6d7ffd0f853515dfd
wa1tnr <wa1tnr@users.noreply.github.com>
2023-02-14 03:01:17 +0000

    fine except delay length calibr
    	modified:   blackpill.fs
    
    On branch dvlp-aa

--- forth/blackpill.fs
+++ forth/blackpill.fs
@@ -1,8 +1,10 @@
-\ Tue 14 Feb 02:38:17 UTC 2023
+\ Tue 14 Feb 02:53:02 UTC 2023
 
 \ PC13 is Black Pill LED - it is blue
 
 ( hex )
+
+hex
 
 1
 VARIABLE speed
@@ -27,9 +29,9 @@ VARIABLE speed
       1 drop
   loop loop loop loop ;
 
-: bdelay 3 delay ; ( -- )
-: bdkdel 8 delay ; ( -- )
-: ldelay 122 delay ; ( -- )
+: bdelay 300 delay ; ( -- ) \ was just 3
+: bdkdel 800 delay ; ( -- ) \ was just 8
+: ldelay 1222 delay ; ( -- ) \ was 122
 : finishmsg ." done." ; ( -- )
 
 : RCC 40023800 ; ( -- addr ) ( p. 65 )
@@ -47,6 +49,70 @@ VARIABLE speed
 
 : GPIOC 40020800 ; ( -- addr )
 ( 2.3 p.65 )
+
+: GPIOC_MODER GPIOC 0 + ; ( -- addr )
+( offset 0x00 8.4.1 p.281 )
+: GPIOC_MODER! ( n -- )
+  GPIOC_MODER @
+  or GPIOC_MODER ! ;
+
+( everything correct March 2021 above this line )
+
+: OUTPUT ( n -- )
+  ( 6 max F min ) \ was C not 6
+  C max E min \ D is PC13
+  2 * 1 swap << ( sets bit 26 for D 13 )
+  GPIOC_MODER! ;
+\ HEX ok
+\ D ok
+\ 2 * ok
+\ 1 SWAP ok
+\ LSHIFT ok
+\ .S  4000000  ok
+\ 2 BASE ! ok
+\ DUP . 100000000000000000000000000 ok
+\ 100000000000000000000000000 local
+
+\ 100 0000 0000 0000 0000 0000 0000 local
+
+: GPIOC_BSRR ( -- addr )
+  GPIOC 18 + ; ( 18 right for all GPIOx is addrs offset )
+
+: BSX 2^ ; ( n -- n )
+: BRX 10 + 2^ ; ( n -- n )
+
+: GPIOC_BSRR!  ( n -- )
+  GPIOC_BSRR ! ;
+
+( : led 6 ; ) \ PD6 convenience selection
+: led D ; \ PC13 onboard LED
+: led!  GPIOC_BSRR! ; ( n -- )
+
+: on BRX led! ; ( n -- )
+: off BSX led! ; ( n -- )
+
+: setupled ( -- )
+  RCC! led OUTPUT led off ;
+
+: blinks ( n -- )
+  depth 1 - 0<
+  if exit then
+  1 - ( normalize )
+  0 do
+    led on
+    bdelay
+    led off
+    bdkdel
+  loop ;
+
+: linit ( -- n )
+  FFFFFF9D setupled
+  3 blinks 
+  7 0 do bdkdel loop
+  7 0 do bdkdel loop
+  led off 3 blinks ;
+
+: nullemit 0 emit ;
 
 
 
